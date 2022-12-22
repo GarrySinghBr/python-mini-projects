b760b793d4acac55087e477dde9caa6cbe3c2779
Garry Brar <76575127+GarrySinghBr@users.noreply.github.com>
2022-12-21 19:48:53 -0500

    Finished Function
    Completed while loop, simple functionality complete.

--- toDoList.py
+++ toDoList.py
@@ -1,24 +1,24 @@
 # A Python To Do List that writes to a text file
 
 def main():
-   print("Welcome to your To Do List!")
+   print("Welcome to your To Do List!\n")
    user = input("What is your name?: ")
    fname = input("What would you like your output file to be called?: ")
    fname = fname + ".txt"
    f = open(fname,"a")
-   f.write("A To-Do-List for " + user +"!")
-   f.write("\n hello world")
-#    while True:
-#         menu = input("What would you like to do? (Add, View, or Quit): ")
-#         if menu == "Add":
-#             add = input("What would you like to add?: ")
-#             file.write(add + "\n")
-#         elif menu == "View":
-#             file = open(fname,"r")
-#             print(file.read())
-#         elif menu == "Quit":
-#             file.close()
-#             print("Goodbye!")
-#             break
-    
+   f.write("To-Do-List for " + user +":\n")
+   while True:
+        menu = input("What would you like to do? (Add, View, or Quit): ")
+        if menu == "Add":
+            add = input("What would you like to add?: ")
+            f.write(add + "\n")
+        elif menu == "View":
+            f = open(fname,"r")
+            print(f.read())
+        elif menu == "Quit":
+            f.close()
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid option! Please try again.")
 main()
