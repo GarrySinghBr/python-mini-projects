b10d05e6ebe349af372eb02d36e0f964e0b4589c
Garry Brar <76575127+GarrySinghBr@users.noreply.github.com>
2022-12-22 18:46:08 -0500

    Update batteryNotif.py

--- batteryNotif.py
+++ batteryNotif.py
@@ -1,2 +1,10 @@
 from plyer import notification
+import psutil
+
+battery = psutil.sensors_battery()
+title = "Battery Low"
+message = "Please connect your charger"
+
+if battery.percent < 30:
+    notification.notify(title = title ,message = message)
 
