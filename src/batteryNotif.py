a26a3fc6c8e5f0bb3e85b0a1db42f57a01095902
Garry Brar <76575127+GarrySinghBr@users.noreply.github.com>
2022-12-23 18:02:12 -0500

    Refactoring
    - New folder to hold src code
    
    - Changed functionality in batteryNotif.py to end after notification

--- src/batteryNotif.py
+++ src/batteryNotif.py
@@ -9,7 +9,9 @@ title = "Battery Low!"
 message = "Please Connect your Charger"
 
 while True:
-    if battery.percent < 90:
+    if battery.percent < 30:
         notification.notify(title = title ,message = message)
+        break
+    else:
         time.sleep(300)
 
