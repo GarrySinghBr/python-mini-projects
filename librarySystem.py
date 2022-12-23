daf7f0e5c2ca02e5c4ffc4f893562fa7eb8f364d
Garry Brar <76575127+GarrySinghBr@users.noreply.github.com>
2022-12-23 17:58:54 -0500

    Update librarySystem.py
    Created simple design flow with menu

--- librarySystem.py
+++ librarySystem.py
@@ -18,16 +18,44 @@ class Library:
         self.list.append(book)
             
     def printBooks(self):
+        print()
         for book in self.list:
             book.print()
-    
-b1 = Book("Harry Potter", "JK Rowling", 500)
-b2 = Book("Lord of the Rings", "Tolkien", 700)
-b3 = Book("The Hobbit", "Tolkien", 300)
+        print()
 
-lib = Library("My Library")
+    def removeBook(self, name):
+        for i in range(len(self.list)):
+            if self.list[i].title == name:
+                self.list.pop(i)
+                print("Book removed!")
+                return
 
-lib.addBook(b1)
-lib.addBook(b2)
-lib.addBook(b3)
 
+def main():
+    print("Welcome to your library!")
+    libName = input("Please enter the name of your library: ")
+    lib = Library(libName)
+    while True:
+        print("1. Add a book")
+        print("2. Remove a book")
+        print("3. Print all books")
+        print("4. Exit")
+        choice = input("Please enter your choice: ")
+        if choice == "1":
+            title = input("Enter the title of the book: ")
+            author = input("Enter the author of the book: ")
+            pages = input("Enter the number of pages in the book: ")
+            book = Book(title, author, pages)
+            lib.addBook(book)
+        elif choice == "2":
+            name = input("Enter the name of the book you want to remove: ")
+            lib.removeBook(name)
+        elif choice == "3":
+            lib.printBooks()
+        elif choice == "4":
+            print("Thank you for using the library system!")
+            break
+        else:
+            print("Invalid choice, please try again!")
+
+main()
